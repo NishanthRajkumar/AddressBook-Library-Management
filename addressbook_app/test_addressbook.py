96c5e892db9cb4dce1d0c736ebdf871f547b4571
Nishanth <rajkumarnishanth@gmail.com>
2022-03-29 23:31:10 +0530

    test 'add multiple contacts' feature

--- addressbook_app/test_addressbook.py
+++ addressbook_app/test_addressbook.py
@@ -29,6 +29,19 @@ class AddressBookTest(unittest.TestCase):
     def test_delete_contact(self):
         self.book.delete_contact(name="Nishanth Rajkumar")
         self.assertFalse("Nishanth Rajkumar" in self.book.contact_list.keys())
+    
+    def test_add_multiple_contacts(self):
+        contact1 = Contact()
+        contact2 = Contact()
+        contact3 = Contact()
+        contact1.create_contact("Sam")
+        contact1.create_contact("Walter")
+        contact3.create_contact("Ralph")
+        contact_list1 = [contact1, contact2]
+        contact_list2 = [contact3, 12, "sggf"]
+        self.assertTrue(self.book.add_multiple_contacts(contact_list1))
+        self.assertRaises(TypeError, self.book.add_multiple_contacts, contact_list2)
+        self.assertRaises(KeyError, self.book.add_multiple_contacts, contact_list1)
 
 
 if __name__ == "__main__":
