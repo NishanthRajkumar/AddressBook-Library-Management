3246c91ae7ce04bf8873c0a5b263f5a456c67508
Nishanth <rajkumarnishanth@gmail.com>
2022-03-29 22:54:31 +0530

    tested the edit contact feature

--- addressbook_app/test_addressbook.py
+++ addressbook_app/test_addressbook.py
@@ -6,17 +6,25 @@
     @Title: Unit testing the address book project
 '''
 import unittest
-from addressbook import AddressBook
+from addressbook import AddressBook, Contact
 
 class AddressBookTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.book = AddressBook()
+        self.book.add_contact("Nishanth", "Rajkumar", phone="9901104972")
+        self.book.add_contact("Jeffy", phone="9952429630")
+
     def test_add_contact(self):
-        book = AddressBook()
-        book.add_contact("Nishanth", "Rajkumar", phone="9901104972")
-        book.add_contact("Jeffy", phone="9952429630")
-
-        self.assertEqual(len(book.contact_list), 2)
-
+        self.assertEqual(len(self.book.contact_list), 2)
+    
+    def test_edit_contact(self):
+        self.book.edit_contact(name="Jeffy", last_name="Raj")
+        expected_contact = Contact()
+        expected_contact.create_contact(first_name="Jeffy", last_name="Raj", phone="9952429630")
+        updated_contact = self.book.contact_list["Jeffy Raj"]
+        self.assertEqual(updated_contact.__str__(), expected_contact.__str__())
+        self.assertRaises(KeyError, self.book.edit_contact, name="Sam", last_name="Raj")
 
 if __name__ == "__main__":
     unittest.main()
